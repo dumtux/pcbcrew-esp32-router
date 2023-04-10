2eb5f94eceb2a1ca5ba6d34c213b321dc354fcd2
BadTux <tekobase@outlook.com>
2023-04-11 02:41:21 +0800

    RTC: test success.

--- main/app_main.c
+++ main/app_main.c
@@ -3,19 +3,21 @@
 #include <stdlib.h>
 
 #include "esp_log.h"
-#include <ds1307.h>
 
 #include "storage.c"
 #include "button.c"
 #include "router.c"
+#include "rtc.c"
 
 static const char *TAG_MAIN = "pcbcrew-router-main";
+
 
 void app_main(void)
 {
     esp_log_level_set("*", ESP_LOG_INFO);
 
     setup_storage();
+    setup_rtc();
     setup_button();
     setup_router();
 }
