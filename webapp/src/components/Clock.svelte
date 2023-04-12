ae4c8687c117ca81c276f1be7c1e06a4bc5246f4
BadTux <tekobase@outlook.com>
2023-04-12 22:53:23 +0800

    webapp: add TailwindCSS and make simple clock component.

---
+++ webapp/src/components/Clock.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { onMount } from 'svelte';
+
+	let time = new Date();
+
+	// these automatically update when `time`
+	// changes, because of the `$:` prefix
+	$: display_date = time.toLocaleDateString();
+	$: display_time = time.toLocaleTimeString();
+
+	onMount(() => {
+		const interval = setInterval(() => {
+			time = new Date();
+		}, 1000);
+
+		return () => {
+			clearInterval(interval);
+		};
+	});
+</script>
+
+<span>{display_date} {display_time}</span>
--- webapp/src/routes/+page.svelte
+++ webapp/src/routes/+page.svelte
@@ -1,21 +1,9 @@
 <script>
-    function buttonClick() {
-        alert("Button clicked");
-    }
+    import "../app.css";
+    import Clock from "../components/Clock.svelte";
 </script>
 
-<h1>Hello World</h1>
-<button class="button" on:click="{buttonClick}">Click!</button>
-
-<style>
-    .button {
-        background-color: #4caf50; /* Green */
-        border: none;
-        color: white;
-        padding: 15px 32px;
-        text-align: center;
-        text-decoration: none;
-        display: inline-block;
-        font-size: 16px;
-    }
-</style>
+<h1 class="text-3xl font-bold underline">
+    PCBCrew ESP32 Router
+</h1>
+<Clock />
